10dc7a710b966d32095f052065a016dffc171e49
FrancoisDuguayG <francois.duguay.g@gmail.com>
2023-02-16 12:52:08 -0500

    Add 13

---
+++ 13-galerie-de-photos/scripts/main.js
@@ -0,0 +1,15 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Déclarer une liste avec les chemins des images*/
+
+/* Déclarer le texte alternative pour chaque image*/
+
+/* traverser les images */
+
+const newImage = document.createElement('img');
+
+/* sombre/clair bouton */
